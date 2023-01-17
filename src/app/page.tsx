2f55d89b89bdfb00c4d66f3f6c3c83db1f0436ea
Adam Rackis <adamrackis@hotmail.com>
2023-01-16 18:11:14 -0600

    Initial poc

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main style={{ margin: "100px" }}>
+      <div>MAIN CONTENT</div>
+    </main>
+  );
+}
